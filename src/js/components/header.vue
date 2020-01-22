14be7f99f4fb21fd1c808786155df83a2e61ddbb
IonutCatalinBadea <ionut.badea@interactively.eu>
2020-01-22 16:13:56 +0200

    design header

--- src/js/components/header.vue
+++ src/js/components/header.vue
@@ -1,34 +1,44 @@
 <template>
-    <header>
-        <ul>
-            <li>
-                <a href="#" @click.stop.prevent="changeLocale('ro')">RO</a>
-            </li>
-            <li>
-                <a href="#" @click.stop.prevent="changeLocale('en')">EN</a>
-            </li>
-            <li>
-                <a href="#" @click.stop.prevent="changeLocale('ja')">JA</a>
-            </li>
-        </ul>
-    </header>
+  <header>
+    <b-row class="flags">
+      <b-col cols="12">
+        <a href="#" @click.stop.prevent="changeLocale('ro')">
+          RO
+        </a>
+
+        <span>|</span>
+
+        <a href="#" @click.stop.prevent="changeLocale('en')">
+          EN
+        </a>
+      </b-col>
+    </b-row>
+
+    <b-row class="title">
+      <b-col cols="12">
+        <h1>
+          <span>Ionut-Catalin BADEA</span>
+          <span>Web Developer</span>
+        </h1>
+      </b-col>
+    </b-row>
+  </header>
 </template>
 
 <script>
+export default {
+  name: "AppHeader",
+  props: {},
+  mounted: function() {},
 
-    export default {
-        name: 'AppHeader',
-        props: {},
-        mounted: function () {
-        },
-        
-methods:{
-    changeLocale: function (locale){
-        Util.i18n_locale(locale);
+  methods: {
+    changeLocale: function(locale) {
+      Util.i18n_locale(locale);
     }
-}
-
-    };
+  }
+};
 </script>
 
-<style scoped lang='scss'></style>
+<style scoped lang="scss">
+
+</style>
